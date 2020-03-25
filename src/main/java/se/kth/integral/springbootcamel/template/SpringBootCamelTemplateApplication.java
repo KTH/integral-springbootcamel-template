76f509423c56fd3505fccb92ca790e2b240ae594
bjfr <bjorn.frantzen@r2m.se>
2020-03-25 11:28:51 +0100

    First version

---
+++ src/main/java/se/kth/integral/springbootcamel/template/SpringBootCamelTemplateApplication.java
@@ -0,0 +1,13 @@
+package se.kth.integral.springbootcamel.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamelTemplateApplication.class, args);
+	}
+
+}
---
+++ src/main/java/se/kth/integral/springbootcamel/template/SpringBootCamelTemplateRoute.java
@@ -0,0 +1,15 @@
+package se.kth.integral.springbootcamel.template;
+
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBootCamelTemplateRoute extends EndpointRouteBuilder {
+
+  @Override
+  public void configure() throws Exception {
+    from(timer("").fixedRate(true).period(1_000))
+        .setBody(constant("Hello World!"))
+        .to(stream("out"));
+  }
+}
